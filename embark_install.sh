7682956ca2d0235588473bbb0e03b8a9b11b5cf4
evanmccarter <evan.mccarter@gmail.com>
2015-10-04 00:36:44 -0400

    added "-y" options to apt calls

--- embark_install.sh
+++ embark_install.sh
@@ -3,31 +3,32 @@
 #basics
 #TODO: ensure apt/aptitude auto-confirm installations
 sudo apt-get update; sudo apt-get install aptitude
-sudo aptitude update; sudo aptitude upgrade
+sudo aptitude update; sudo aptitude -y upgrade
 
 #install git
-sudo aptitude install git
+sudo aptitude install -y git
 
 #npm, n, node
-sudo aptitude install npm
+sudo aptitude install -y npm
 sudo npm install -g n
 sudo n 0.12.2
 sudo n 0.12.2
 
 #install geth
-sudo aptitude install software-properties-common
+sudo aptitude install -y software-properties-common
 sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo aptitude update
-sudo aptitude install ethereum
+sudo aptitude install -y ethereum
 echo -e "y\nexit\n" | geth console
 echo -e "personal.newAccount(\"123\")\nexit\n" | geth console
 
 #install CPP-ethereum
-sudo add-apt-repository ppa:ethereum/ethereum-qt
-sudo add-apt-repository ppa:ethereum/ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum-qt
+sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo aptitude update
-sudo aptitude install cpp-ethereum
+sudo aptitude install -y cpp-ethereum
 
 
 #install embark
 sudo npm install -g embark-framework grunt-cli
+
